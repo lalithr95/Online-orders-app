f7ff8dfb546b67222c26b69db54bfe2d302952b3
Lalith <lalithr95@gmail.com>
2015-09-27 01:27:38 +0530

    Cart without localstorage

--- app/scripts/controllers/menudata.js
+++ app/scripts/controllers/menudata.js
@@ -9,8 +9,10 @@
 angular.module('theFirstMealApp')
     .controller('MenudatactrlCtrl', ['$scope', 'getMenuData', 'appSettings', '$timeout', '$window', function ($scope, getMenuData, appSettings, $timeout, $window) {
         $scope.menuData = {};
+        $scope.cart = {};
         $scope.appSettings = appSettings;
         $scope.loading = true;
+        console.log(0);
         $timeout(function () {
             getMenuData()
                 .success(function (response) {
@@ -22,4 +24,26 @@ angular.module('theFirstMealApp')
                     $scope.loading = false;
                 });
         }, 0);
+        $scope.count = 0;
+        $scope.addItem = function(index) {
+            if ( $scope.cart[$scope.menuData.items[index].name] > 0) {
+                $scope.cart[$scope.menuData.items[index].name]++;
+            }
+            else {
+                $scope.cart[$scope.menuData.items[index].name] = 1;
+            }
+            $scope.count++;
+            console.log($scope.cart);
+        };
+        $scope.addPackage = function(index) {
+            if ( $scope.cart[$scope.menuData.packages[index].name] > 0) {
+                $scope.cart[$scope.menuData.packages[index].name]++;
+            }
+            else {
+                $scope.cart[$scope.menuData.packages[index].name] = 1;
+            }
+            $scope.count++;
+            console.log($scope.cart);  
+        };
+        //TODO Need to implement the counter part
   }]);
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -45,6 +45,7 @@ module.exports = function(config) {
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-touch/angular-touch.js',
+      'app/bower_components/ngCart/dist/ngCart.js',
       'app/bower_components/ngCordova/dist/ng-cordova.js',
       'app/bower_components/ngstorage/ngStorage.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
